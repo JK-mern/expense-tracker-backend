c1ca4a5388893bd3ee69bb24e1c81b146e9262ef
JK-mern <jkmerndev@gmail.com>
2025-10-21 12:14:18 +0530

    feat: auth middleware

--- src/schemas/auth/auth.schema.ts
+++ src/schemas/auth/auth.schema.ts
@@ -6,8 +6,6 @@ export const checkUserExist = z.object({
 
 export const createUser = z.object({
   currentBalance: z.string('Current balance is required'),
-  email: z.email('Email is required'),
-  id: z.string('id is required'),
   profilePicture: z.string().optional(),
   userName: z.string('username is required'),
 });
