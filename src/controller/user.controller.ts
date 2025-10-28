f942cf7a753010f8a54984dee51483b44e8f0e73
JK-mern <jkmerndev@gmail.com>
2025-10-28 17:12:52 +0530

    feat: user controller and get current user api

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,44 @@
+import type {NextFunction, Request, Response} from 'express';
+
+import {PrismaClient} from '@prisma/client';
+
+import {Logger} from '../logger/logger.js';
+
+export class UserController {
+  private logger = Logger.getInstance();
+  private prismaClient: PrismaClient;
+
+  constructor() {
+    this.prismaClient = new PrismaClient();
+  }
+
+  public getCurrentUserDetails = async (
+    req: Request,
+    res: Response,
+    _next: NextFunction,
+  ) => {
+    try {
+      const userId = req.user?.id;
+
+      if (!userId) {
+        return res.status(404).json({msg: 'unauthorized user'});
+      }
+
+      const userInfo = await this.prismaClient.user.findUnique({
+        select: {
+          email: true,
+          profilePicture: true,
+          userName: true,
+        },
+        where: {
+          id: userId,
+        },
+      });
+
+      res.status(200).json({data: userInfo, status: true});
+    } catch (error) {
+      this.logger.error(error);
+      res.status(500).json({status: false});
+    }
+  };
+}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import {App} from './app.js';
 import {BalanceRoutes} from './routes/balance.route.js';
 import {AuthRoutes} from './routes/index.js';
+import {UserRoute} from './routes/user.route.js';
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3500;
-const routes = [new AuthRoutes().route, new BalanceRoutes().route];
+const routes = [
+  new AuthRoutes().route,
+  new BalanceRoutes().route,
+  new UserRoute().route,
+];
 const app = new App(port, routes);
 app.listen();
